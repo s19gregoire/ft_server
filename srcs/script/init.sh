363e2f2a48968a909a6190b51486b0f250c408d7
neve <gneve@student.s19.be>
2021-04-21 15:51:20 +0000

    gregoire

--- srcs/script/init.sh
+++ srcs/script/init.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get install -y procps && apt-get install nano && apt-get install -y wget
-
+apt-get install dialog apt-utils -y
 apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
 apt-get -y install wget
 apt-get -y install nginx
